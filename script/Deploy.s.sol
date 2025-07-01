9c33b27b451b5379d0c7f1832feb703d72399ade
AleksDemian <81850543+AleksDemian@users.noreply.github.com>
2025-07-01 18:14:37 +0300

    Connector (#1)
    * create strBTC-SY and strBTC-SY-Upgradeable connectors
    
    * use forge fmt
    
    * fix optimize problem
    
    * create test mock
    
    * create test mock

---
+++ script/Deploy.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "forge-std/Script.sol";
+import "../src/PendleStrBTCSY.sol";
+
+contract DeployScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        address strBTC = vm.envAddress("STR_BTC_ADDRESS");
+        address wstrBTC = vm.envAddress("WSTR_BTC_ADDRESS");
+        address wBTC = vm.envAddress("WBTC_ADDRESS");
+        address wBTCConverter = vm.envAddress("WBTC_CONVERTER_ADDRESS");
+
+        string memory name = vm.envOr("TOKEN_NAME", string("SY strBTC"));
+        string memory symbol = vm.envOr("TOKEN_SYMBOL", string("SY-strBTC"));
+
+        vm.startBroadcast();
+
+        PendleStrBTCSY pendleStrBTCSY = new PendleStrBTCSY(name, symbol, strBTC, wstrBTC, wBTC, wBTCConverter);
+
+        vm.stopBroadcast();
+
+        console.log("PendleStrBTCSY deployed to:", address(pendleStrBTCSY));
+    }
+}
---
+++ test/PendleStrBTCSYTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/PendleStrBTCSY.sol";
+
+contract PendleStrBTCSYTest is Test {
+    PendleStrBTCSY public sy;
+
+    address public alice = makeAddr("alice");
+    address public bob = makeAddr("bob");
+
+    function setUp() public {}
+
+    function testDepositWstrBTC() public {}
+
+    function testDepositStrBTC() public {}
+}
